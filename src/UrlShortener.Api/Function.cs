8eb8794205eb6d00013cc8003a6e97adcb641ea1
Harjeet Singh <harjeetsodhihss16@gmail.com>
2026-06-09 09:43:20 +0530

    Refactor FunctionHandler to return APIGatewayProxyResponse

--- src/UrlShortener.Api/Function.cs
+++ src/UrlShortener.Api/Function.cs
@@ -1,3 +1,4 @@
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -7,15 +8,14 @@ namespace UrlShortener.Api;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public APIGatewayProxyResponse FunctionHandler(
+        APIGatewayProxyRequest request,
+        ILambdaContext context)
     {
-        return input.ToUpper();
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = "URL Shortener API is healthy"
+        };
     }
 }
